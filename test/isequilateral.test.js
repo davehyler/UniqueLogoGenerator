b3a2bc05460bb68b997dcfe0620ecda83828e79d
davehyler <164968520+davehyler@users.noreply.github.com>
2024-08-29 19:01:31 -0700

    Delete test directory

--- test/isequilateral.test.js
+++
@@ -1,8 +0,0 @@
-const {Triangle} = require('../generatelogo.js');
-
-describe('generateSVG', () => {
-  test('verifyTriangleIsEquilateral', () => {
-    const shape = Triangle;
-    expect(shape.render()).toEqual('<polygon points="0, 260 150, 0 300, 260"');
-  });
-});
